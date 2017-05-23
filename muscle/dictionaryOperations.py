9ad30067145adb8c8787f1ae2817e789dafcc588
musale <musale@focusmobile.co>
2017-05-23 09:46:31 +0300

    Pop values from 2 or more lists and append into a new list.

--- muscle/dictionaryOperations.py
+++ muscle/dictionaryOperations.py
@@ -1,1 +1,22 @@
 """Exercises that would require dictionary exercise."""
+
+# 1. We have two sorted lists, and we want to write a function to merge
+# the two lists into one sorted list
+a = [3, 4, 6, 10, 11, 18]
+b = [1, 5, 7, 12, 13, 19, 21]
+
+
+# 1a. We can pop the values into a new list and then merge with + operator
+def popValues(firstList, secondList):
+    """Get the firstList and secondList and then pop the values."""
+    newList = []
+    while firstList and secondList:
+        if firstList[0] < secondList[0]:
+            newList.append(firstList.pop(0))
+        else:
+            newList.append(secondList.pop(0))
+    return newList + firstList + secondList
+
+
+if __name__ == '__main__':
+    print popValues(a, b)
